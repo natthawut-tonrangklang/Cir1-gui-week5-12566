bd9aa587efb52866e224a70aa72e348725c1e617
natthawut-tonrangklang <hanxy468@gmail.com>
2023-06-13 13:13:43 +0700

    update

--- bin/dart_week5.dart
+++ bin/dart_week5.dart
@@ -1,8 +1,8 @@
 void main() {
   print("Assignment operators");
   print("==========");
-  const int n1 = "10";
-  const int n2 = "2";
+  const int n1 = 10;
+  const int n2 = 2;
   print("n1+n2 = ${n1 + n2}");
   print("n1-n2 = ${n1 - n2}");
   print("n1*n2 = ${n1 * n2}");
